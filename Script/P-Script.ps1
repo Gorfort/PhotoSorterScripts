d87bab316077961f9ed4f0b9702fe44e9788d2ef
Thibaud <94205515+Gorfort@users.noreply.github.com>
2023-12-15 11:46:13 +0100

    Added color to text & handle more issues

--- Script/P-Script.ps1
+++ Script/P-Script.ps1
@@ -1,30 +1,60 @@
+# Change the color of "Welcome" to blue
+Write-Host "Welcome" -ForegroundColor Blue
+ 
+# Function to prompt the user for folder path and handle invalid input
+function Get-FolderPath {
+    param (
+        [string]$prompt
+    )
+    do {
+        $folderPath = Read-Host -Prompt $prompt
+        if (-not (Test-Path $folderPath -PathType Container)) {
+            Write-Host "Invalid folder path. Please try again." -ForegroundColor Red
+        }
+        else {
+            break
+        }
+    } while ($true)
+ 
+    return $folderPath
+}
+ 
+# Prompt the user for the source folder
+$sourceFolder = Get-FolderPath -prompt "Enter the source folder path " 
+ 
 # Prompt the user for the folder name
 $folderName = Read-Host -Prompt "Enter folder name"
  
-# Create the destination folder
-$destinationFolder = Join-Path -Path "C:\Users\EtmlPowershell\Desktop\photos" -ChildPath $folderName
-New-Item -ItemType Directory -Path $destinationFolder
+# Prompt the user for the destination folder
+$destinationFolder = Get-FolderPath -prompt "Enter the destination folder path "
+ 
+# Check if the destination folder exists, if not, create it
+if (-not (Test-Path $destinationFolder -PathType Container)) {
+    New-Item -ItemType Directory -Path $destinationFolder | Out-Null
+}
+ 
+# Create the destination folder with the specified folder name
+$destinationFolder = Join-Path -Path $destinationFolder -ChildPath $folderName
+New-Item -ItemType Directory -Path $destinationFolder | Out-Null
  
 # Create RAW and JPEG folders inside the destination folder
 $rawFolderPath = Join-Path -Path $destinationFolder -ChildPath "RAW"
 $jpegFolderPath = Join-Path -Path $destinationFolder -ChildPath "JPEG"
  
-New-Item -ItemType Directory -Path $rawFolderPath
-New-Item -ItemType Directory -Path $jpegFolderPath
+New-Item -ItemType Directory -Path $rawFolderPath | Out-Null
+New-Item -ItemType Directory -Path $jpegFolderPath | Out-Null
  
-# Get all the photos from the SD card (excluding subdirectories)
-$photos = Get-ChildItem -Path "C:\Users\EtmlPowershell\Desktop\SD" 
+# Get all the photos from the source folder (excluding subdirectories)
+$photos = Get-ChildItem -Path $sourceFolder
  
 foreach ($photo in $photos) {
-    
-    if ($photo.Extension -eq ".CR3")
-    {
-        Copy-Item $photo $rawFolderPath
+    if ($photo.Extension -eq ".CR3") {
+        Copy-Item $photo.FullName $rawFolderPath
     }
-    if ($photo.Extension -eq ".JPEG")
-    {
-        Copy-Item $photo $jpegFolderPath
+    elseif ($photo.Extension -eq ".JPEG" -or $photo.Extension -eq ".JPG") {
+        Copy-Item $photo.FullName $jpegFolderPath
     }
 }
  
-Write-Host "Photos have been copied successfully."
+# Change the color of "Photos have been copied successfully" to green
+Write-Host "Photos have been copied successfully." -ForegroundColor Green
