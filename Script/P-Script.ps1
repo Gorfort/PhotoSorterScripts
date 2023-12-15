f0f4f3aa6ba507bee7eb7c4a260a25287b7df03d
Thibaud <94205515+Gorfort@users.noreply.github.com>
2023-12-15 12:05:55 +0100

    Update P-Script.ps1

--- Script/P-Script.ps1
+++ Script/P-Script.ps1
@@ -1,6 +1,5 @@
 # Change the color of "Welcome" to blue
 Write-Host "Welcome" -ForegroundColor Blue
- 
 # Function to prompt the user for folder path and handle invalid input
 function Get-FolderPath {
     param (
@@ -15,24 +14,35 @@ function Get-FolderPath {
             break
         }
     } while ($true)
- 
     return $folderPath
 }
  
+# Function to prompt the user for a non-empty folder name
+function Get-FolderName {
+    param (
+        [string]$prompt
+    )
+    do {
+        $folderName = Read-Host -Prompt $prompt
+        if (-not $folderName) {
+            Write-Host "Folder name cannot be empty. Please enter a name." -ForegroundColor Red
+        }
+        else {
+            break
+        }
+    } while ($true)
+    return $folderName
+}
 # Prompt the user for the source folder
-$sourceFolder = Get-FolderPath -prompt "Enter the source folder path " 
- 
+$sourceFolder = Get-FolderPath -prompt "Enter the source folder path "
 # Prompt the user for the folder name
-$folderName = Read-Host -Prompt "Enter folder name"
- 
+$folderName = Get-FolderName -prompt "Enter folder name"
 # Prompt the user for the destination folder
 $destinationFolder = Get-FolderPath -prompt "Enter the destination folder path "
- 
 # Check if the destination folder exists, if not, create it
 if (-not (Test-Path $destinationFolder -PathType Container)) {
     New-Item -ItemType Directory -Path $destinationFolder | Out-Null
 }
- 
 # Create the destination folder with the specified folder name
 $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $folderName
 New-Item -ItemType Directory -Path $destinationFolder | Out-Null
