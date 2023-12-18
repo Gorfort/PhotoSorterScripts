913cf9ecd67c3b33514bdd5c31360e5fc320ea3d
Thibaud <94205515+Gorfort@users.noreply.github.com>
2023-12-18 20:54:27 +0100

    Added progress bar

--- Script/P-Script.ps1
+++ Script/P-Script.ps1
@@ -1,5 +1,6 @@
 # Change the color of "Welcome" to blue
 Write-Host "Welcome" -ForegroundColor Blue
+
 # Function to prompt the user for folder path and handle invalid input
 function Get-FolderPath {
     param (
@@ -16,7 +17,7 @@ function Get-FolderPath {
     } while ($true)
     return $folderPath
 }
- 
+
 # Function to prompt the user for a non-empty folder name
 function Get-FolderName {
     param (
@@ -33,30 +34,39 @@ function Get-FolderName {
     } while ($true)
     return $folderName
 }
+
 # Prompt the user for the source folder
 $sourceFolder = Get-FolderPath -prompt "Enter the source folder path "
+
 # Prompt the user for the folder name
 $folderName = Get-FolderName -prompt "Enter folder name"
+
 # Prompt the user for the destination folder
 $destinationFolder = Get-FolderPath -prompt "Enter the destination folder path "
+
 # Check if the destination folder exists, if not, create it
 if (-not (Test-Path $destinationFolder -PathType Container)) {
     New-Item -ItemType Directory -Path $destinationFolder | Out-Null
 }
+
 # Create the destination folder with the specified folder name
 $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $folderName
 New-Item -ItemType Directory -Path $destinationFolder | Out-Null
- 
+
 # Create RAW and JPEG folders inside the destination folder
 $rawFolderPath = Join-Path -Path $destinationFolder -ChildPath "RAW"
 $jpegFolderPath = Join-Path -Path $destinationFolder -ChildPath "JPEG"
- 
+
 New-Item -ItemType Directory -Path $rawFolderPath | Out-Null
 New-Item -ItemType Directory -Path $jpegFolderPath | Out-Null
- 
+
 # Get all the photos from the source folder (excluding subdirectories)
 $photos = Get-ChildItem -Path $sourceFolder
- 
+
+# Initialize variables for the progress bar
+$totalFiles = $photos.Count
+$processedFiles = 0
+
 foreach ($photo in $photos) {
     if ($photo.Extension -eq ".CR3") {
         Copy-Item $photo.FullName $rawFolderPath
@@ -64,7 +74,12 @@ foreach ($photo in $photos) {
     elseif ($photo.Extension -eq ".JPEG" -or $photo.Extension -eq ".JPG") {
         Copy-Item $photo.FullName $jpegFolderPath
     }
+
+    # Update the progress bar
+    $processedFiles++
+    $percentComplete = [math]::floor(($processedFiles / $totalFiles) * 100)
+    Write-Progress -Activity "Copying Files" -PercentComplete $percentComplete -Status "$processedFiles/$totalFiles files copied - $percentComplete% complete"
 }
- 
+Clear-Host
 # Change the color of "Photos have been copied successfully" to green
 Write-Host "Photos have been copied successfully." -ForegroundColor Green
