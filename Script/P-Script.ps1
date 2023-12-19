91958f5a1765d07196b45c42f7e7932c0dc91cb2
Thibaud <94205515+Gorfort@users.noreply.github.com>
2023-12-19 09:31:15 +0100

    Handle Video Files
    Now the script can hande .mp4 and .MOV video formats

--- Script/P-Script.ps1
+++ Script/P-Script.ps1
@@ -1,6 +1,6 @@
 # Change the color of "Welcome" to blue
 Write-Host "Welcome" -ForegroundColor Blue
-
+ 
 # Function to prompt the user for folder path and handle invalid input
 function Get-FolderPath {
     param (
@@ -17,7 +17,7 @@ function Get-FolderPath {
     } while ($true)
     return $folderPath
 }
-
+ 
 # Function to prompt the user for a non-empty folder name
 function Get-FolderName {
     param (
@@ -34,52 +34,89 @@ function Get-FolderName {
     } while ($true)
     return $folderName
 }
-
-# Prompt the user for the source folder
-$sourceFolder = Get-FolderPath -prompt "Enter the source folder path "
-
-# Prompt the user for the folder name
-$folderName = Get-FolderName -prompt "Enter folder name"
-
-# Prompt the user for the destination folder
-$destinationFolder = Get-FolderPath -prompt "Enter the destination folder path "
-
-# Check if the destination folder exists, if not, create it
-if (-not (Test-Path $destinationFolder -PathType Container)) {
+ 
+# Function to ask the user if they want to run the script again
+function AskRunAgain {
+    do {
+        $choice = Read-Host "Do you want to run the script again? (Y/N)"
+        if ($choice -eq 'Y' -or $choice -eq 'N') {
+            return $choice
+        }
+        else {
+            Write-Host "Invalid choice. Please enter Y or N." -ForegroundColor Red
+        }
+    } while ($true)
+}
+ 
+do {
+    # Prompt the user for the source folder
+    $sourceFolder = Get-FolderPath -prompt "Enter the source folder path "
+ 
+    # Prompt the user for the folder name
+    $folderName = Get-FolderName -prompt "Enter folder name"
+ 
+    # Prompt the user for the destination folder
+    $destinationFolder = Get-FolderPath -prompt "Enter the destination folder path "
+ 
+    # Check if the destination folder exists, if not, create it
+    if (-not (Test-Path $destinationFolder -PathType Container)) {
+        New-Item -ItemType Directory -Path $destinationFolder | Out-Null
+    }
+ 
+    # Create the destination folder with the specified folder name
+    $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $folderName
     New-Item -ItemType Directory -Path $destinationFolder | Out-Null
-}
-
-# Create the destination folder with the specified folder name
-$destinationFolder = Join-Path -Path $destinationFolder -ChildPath $folderName
-New-Item -ItemType Directory -Path $destinationFolder | Out-Null
-
-# Create RAW and JPEG folders inside the destination folder
-$rawFolderPath = Join-Path -Path $destinationFolder -ChildPath "RAW"
-$jpegFolderPath = Join-Path -Path $destinationFolder -ChildPath "JPEG"
-
-New-Item -ItemType Directory -Path $rawFolderPath | Out-Null
-New-Item -ItemType Directory -Path $jpegFolderPath | Out-Null
-
-# Get all the photos from the source folder (excluding subdirectories)
-$photos = Get-ChildItem -Path $sourceFolder
-
-# Initialize variables for the progress bar
-$totalFiles = $photos.Count
-$processedFiles = 0
-
-foreach ($photo in $photos) {
-    if ($photo.Extension -eq ".CR3") {
-        Copy-Item $photo.FullName $rawFolderPath
+ 
+    # Create RAW, JPEG, and Video folders inside the destination folder
+    $rawFolderPath = Join-Path -Path $destinationFolder -ChildPath "RAW"
+    $jpegFolderPath = Join-Path -Path $destinationFolder -ChildPath "JPEG"
+    $videoFolderPath = Join-Path -Path $destinationFolder -ChildPath "Video"
+ 
+    New-Item -ItemType Directory -Path $rawFolderPath | Out-Null
+    New-Item -ItemType Directory -Path $jpegFolderPath | Out-Null
+    New-Item -ItemType Directory -Path $videoFolderPath | Out-Null
+ 
+    # Get all the photos from the source folder (excluding subdirectories)
+    $photos = Get-ChildItem -Path $sourceFolder
+ 
+    # Initialize variables for the progress bar
+    $totalFiles = $photos.Count
+    $processedFiles = 0
+ 
+    foreach ($photo in $photos) {
+        if ($photo.Extension -eq ".CR3") {
+            Copy-Item $photo.FullName $rawFolderPath
+        }
+        elseif ($photo.Extension -eq ".JPEG" -or $photo.Extension -eq ".JPG") {
+            Copy-Item $photo.FullName $jpegFolderPath
+        }
+        elseif ($photo.Extension -eq ".mp4" -or $photo.Extension -eq ".MOV") {
+            Copy-Item $photo.FullName $videoFolderPath
+        }
+ 
+        # Update the progress bar
+        $processedFiles++
+        $percentComplete = [math]::floor(($processedFiles / $totalFiles) * 100)
+        Write-Progress -Activity "Copying Files" -PercentComplete $percentComplete -Status "$processedFiles/$totalFiles files copied - $percentComplete% complete"
     }
-    elseif ($photo.Extension -eq ".JPEG" -or $photo.Extension -eq ".JPG") {
-        Copy-Item $photo.FullName $jpegFolderPath
+ 
+    Clear-Host
+    # Change the color of "Photos have been copied successfully" to green
+    Write-Host "Photos have been copied successfully." -ForegroundColor Green
+ 
+    # Check if folders are empty and delete them
+    if ((Get-ChildItem $rawFolderPath | Measure-Object).Count -eq 0) {
+        Remove-Item $rawFolderPath
     }
-
-    # Update the progress bar
-    $processedFiles++
-    $percentComplete = [math]::floor(($processedFiles / $totalFiles) * 100)
-    Write-Progress -Activity "Copying Files" -PercentComplete $percentComplete -Status "$processedFiles/$totalFiles files copied - $percentComplete% complete"
-}
-Clear-Host
-# Change the color of "Photos have been copied successfully" to green
-Write-Host "Photos have been copied successfully." -ForegroundColor Green
+ 
+    if ((Get-ChildItem $jpegFolderPath | Measure-Object).Count -eq 0) {
+        Remove-Item $jpegFolderPath
+    }
+ 
+    if ((Get-ChildItem $videoFolderPath | Measure-Object).Count -eq 0) {
+        Remove-Item $videoFolderPath
+    }
+ 
+    # Ask the user if they want to run the script again
+    $runAgain = AskRunAgain
+} while ($runAgain -eq 'Y')
